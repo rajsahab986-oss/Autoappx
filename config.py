1ed374c703cf38d393f1861f1d51ac62a833e849
rajsahab986-oss <rajsahab986@gmail.com>
2025-11-01 16:04:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,14 @@
-😂
+import os
+##Code Written By @ItsMeMaster
+##Code Written By @ItsMeMaster
+
+class Config(object):
+    BOT_TOKEN = "8434843795:AAFamCgCGaQXXvTDkJ4FWpHnu_Dv4MmKxxE"
+    DB_NAME = "Yashxug"
+    API_ID = "22849789"
+    API_HASH = "0fc127c6055acd59f00ec6c229e1e3c4"
+    ADMIN_ID = 7296271316
+    DB_URL = "mongodb+srv://yashxug:Yash2835P@yashxug.om3yk1q.mongodb.net/retryWrites=true&w=majority&appName=Yashxug"
+    LOG_CHANNEL = "-1002477114210" # Your Log Channel ID (Bot ko ADMIN BNAYE)
+    USERLINK = ""
+    TUTORIAL_VIDEO = ""
